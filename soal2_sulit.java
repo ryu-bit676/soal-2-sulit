55b2b412348b3595a1b335e2dca4d58a921e9ac5
ryu-bit676 <achmad_mariadi_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 16:38:28 +0700

    Add files via upload

---
+++ soal2_sulit.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class soal2_sulit {
+    
+    public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris matriks: ");
+        int baris = input.nextInt();
+        System.out.print("Masukkan jumlah kolom matriks: ");
+        int kolom = input.nextInt();
+
+        int[][] matriksA = new int[baris][kolom];
+        int[][] matriksB = new int[baris][kolom];
+        int[][] hasil = new int[baris][kolom];
+
+        System.out.println("\nMasukkan elemen-elemen Matriks A:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                matriksA[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("\nMasukkan elemen-elemen Matriks B:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("B[" + i + "][" + j + "] = ");
+                matriksB[i][j] = input.nextInt();
+            }
+        }
+
+        // Proses penjumlahan matriks
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                hasil[i][j] = matriksA[i][j] + matriksB[i][j];
+            }
+        }
+
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan Matriks A + Matriks B:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(hasil[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
+    
+
